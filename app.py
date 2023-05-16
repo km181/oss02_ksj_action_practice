ffee293159d0205d63ae4f9ba2548c1e8cfa6ede
ksj000 <126738968+km181@users.noreply.github.com>
2023-05-16 12:47:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37,.76, -122.4], columns
 
 st.map(df)
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x*x)
+#x = st.slider('Select a value')
+#st.write(x, 'squared is', x*x)
+
+rand = np.random.normal(1, 2, size= 20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
